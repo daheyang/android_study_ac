8c7f389ed573ff5605fa7ea1f0042ff4482aa97a
Dahe <subwayang00@gmail.com>
2021-11-06 12:18:56 +0900

    로그인 화면

---
+++ assg1/app/src/main/java/org/daheyang/study_assg1/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.daheyang.study_assg1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
